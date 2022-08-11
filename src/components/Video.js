2f1493d70db9c6a6178c64f855735bb7cefd5b56
Abhiram.adapaka <abhiramadapaka@gmail.com>
2022-08-11 11:38:54 +0530

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import axios from 'axios'
 
-const Video = ({ srcLink, id,width,height }) => {
+const Video = ({ srcLink, id, width, height }) => {
+  let config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    }
+  }
  
   function history(id) {
 
-    axios.get(`http://localhost:3001/Data/${id}`).then(response => { 
-      axios.post('http://localhost:3001/History',
+    axios.get(`https://api.npoint.io/baf4bcf9eb8946410e5e/Data/${id}`,config).then(response => { 
+      axios.post('https://api.npoint.io/baf4bcf9eb8946410e5e/History',
         {
           "VideoId": response.data.id,
           "time": new Date().toLocaleString()
-        }
+        },config
       ).then(response => { console.log("posted", response) }).catch(error => { console.log(error) })
 
      }).catch(error => { console.log(error) })
